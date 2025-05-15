29681bc44977195746f3b46e516f036d2c15d441
amkadian <amkadian@deloitte.com>
2025-05-15 18:33:12 -0400

    Final commit6

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -7,5 +7,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello this is Lambda! From Amresh trying to deploy using CDK')
+        'body': json.dumps('Hello this is Lambda! From Amresh trying to deploy using CDK.')
     }
